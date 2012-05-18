db9bb3eaceec6ed23dd61be22006ab4ad423532d
Rayjan Wilson <rayjan.wilson@gmail.com>
2012-05-18 06:41:04 -0800

    added directory handling and recursive conversion to the photo script

--- undistort-gopro-hero2-photo.py
+++ undistort-gopro-hero2-photo.py
@@ -1,14 +1,10 @@
-import  sys
-import  cv
-import  numpy as np
- 
-if  __name__ ==  '__main__' :
- 
-    img =  cv.LoadImageM( 'gopro0467-small.jpeg'  )
- 
-    width =  img.width
-    height =  img.height
- 
+
+import sys
+import os, errno
+import cv
+import numpy as np
+
+def setupCV() :
     # Camera data for GoPro Hero2
     camera_matrix =  cv.CreateMat ( 3 , 3 , cv.CV_32FC1)
     cv.SetReal2D (camera_matrix, 0 , 0 , 767.4898 )
@@ -27,20 +23,48 @@ if  __name__ ==  '__main__' :
     cv.SetReal2D (dist_coeffs, 0 , 2 , - 0.00171211222187 )
     cv.SetReal2D (dist_coeffs, 0 , 3 , 0.00142017123289 )
     cv.SetReal2D (dist_coeffs, 0 , 4 , - 0.000823813956231 )
+
+    return [camera_matrix,dist_coeffs]
+
+def undistort(srcImage, destImage, camera_matrix, dist_coeffs) :
+    img =  cv.LoadImageM( srcImage )
  
- 
+    width =  img.width
+    height =  img.height
+
     map1 =  cv.CreateImage ((width, height), cv.IPL_DEPTH_32F, 1 )
     map2 =  cv.CreateImage ((width, height), cv.IPL_DEPTH_32F, 1 )
     cv.InitUndistortMap (camera_matrix, dist_coeffs, map1, map2)
  
+    undistImage =  cv.CloneMat (img)
+    cv.Remap (img, undistImage, map1, map2)
+    cv.SaveImage(destImage, undistImage)
+    print "\tSaved to: ", destImage
+
+def getOutputFileName(fileName):
+    pre = 'undist-'
+    ext = 'jpg'
+    outputFileName = pre + fileName + '.' + ext
+    return outputFileName
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
+ 
+if  __name__ ==  '__main__' :
+    camera_matrix, dist_coeffs = setupCV()
+    directory = sys.argv[1]
+    mkdir_p(directory + 'undistorted')
     
-    undistimage =  cv.CloneMat (img)
-    cv.Remap (img, undistimage, map1, map2)
+    for somefile in os.listdir(directory):
+        fileName, fileExtension = os.path.splitext(somefile)
+        fileExtension = fileExtension[1:].strip()
+        if (fileExtension in ['jpg', 'jpeg', 'JPG']):
+            print "Converting: " + somefile
+            destImage = getOutputFileName(fileName)
+            undistort(directory + somefile, directory + 'undistorted/' + destImage, camera_matrix, dist_coeffs)
 
-    cv.NamedWindow("Source", 1)
-    cv.ShowImage("Source", img)
-
-    cv.NamedWindow("Undistorted", 1)
-    cv.ShowImage("Undistorted", undistimage)
-
-    cv.WaitKey(0)
